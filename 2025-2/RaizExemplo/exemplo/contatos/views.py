f77abf29321515405de221c90a0910d67329d21d
AlexandreMeslin <meslin@puc-rio.br>
2025-09-09 23:22:15 +0000

    2025-09-09

--- 2025-2/RaizExemplo/exemplo/contatos/views.py
+++ 2025-2/RaizExemplo/exemplo/contatos/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from contatos.models import Pessoa
+from contatos.forms import ContatoModel2Form
 
 # Create your views here.
+
+class ContatoCreateView(View):
+    def get(self, request, *args, **kwargs):
+        contexto = {'form': ContatoModel2Form, }
+        return render(request, 'contatos/cadastroContato.html', contexto)
 
 class ContatoListView(View):
     def get(self, request, *args, **kwargs):
         # recupera todas as pessoas do banco de dados
-        pessoas = Pessoa.objects.all()  
+        pessoas = Pessoa.objects.all().order_by('nome')
 
         # contexto para o template
         # dicionário contexto
         # chave 'pessoas'
         # valor da chave é o objecto com todas as pessoas
         contexto = {'pessoas': pessoas}
-        return render(request, 'pessoas/listaContatos.html', contexto)
+        return render(request, 'contatos/listaContatos.html', contexto)
--- 2025-2/RaizExemplo/exemplo/exemplo/settings.py
+++ 2025-2/RaizExemplo/exemplo/exemplo/settings.py
@@ -107,9 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
+TIME_ZONE = 'America/Sao_Paulo' 
+LANGUAGE_CODE = 'pt-BR'
 
 USE_I18N = True
 
