e2accef6897547523e8bf8d328c1d1e771215f45
AlexandreMeslin <meslin@puc-rio.br>
2025-10-09 11:46:14 +0000

    2025-10-09

---
+++ 2025-2/TypeScript/public/script/TiposDeVariaveis.js
@@ -0,0 +1,10 @@
+"use strict";
+// Exemplos de constantes
+// Constantes do tipo string
+const TEXTO = "Um texto";
+const OUTRO_TEXTO = "Um outro texto";
+//TEXTO = 'Mudou o valor da constante';
+let nome;
+nome = "Claudia";
+nome = "Kátia";
+console.log("um texto");
---
+++ 2025-2/TypeScript/typescript/TiposDeVariaveis.ts
@@ -0,0 +1,15 @@
+// Novos tipos
+type NumberOrString = number | string;
+
+// Exemplos de constantes
+// Constantes do tipo string
+const TEXTO: string = "Um texto";
+const OUTRO_TEXTO = "Um outro texto"
+
+//TEXTO = 'Mudou o valor da constante';
+
+let nome: string;
+
+nome = "Claudia";
+nome = "Kátia";
+console.log("um texto");
